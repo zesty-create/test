a3b04c4be6fdf4e6afd9f0241653ac5669b7056d
zesty-create <196483494+zesty-create@users.noreply.github.com>
2026-06-03 16:27:03 +0300

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -3100,7 +3100,7 @@ function Library:CreateWindow(...)
         BorderSizePixel = 0;
         Size = UDim2.new(1, 0, 1, 0);
         Position = UDim2.new(0, 0, 0, 0);
-        Image = 'rbxassetid://78711824094335';
+        Image = 'rbxassetid://86666015360962';
         ScaleType = Enum.ScaleType.Crop;
         ZIndex = 1;
         Parent = Inner;
